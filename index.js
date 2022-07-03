dfbb39f148379e51afcddf153e5bcbe305e3efbb
Craig Hyman <misccorp@yahoo.com>
2022-07-03 15:56:53 -0400

    section options

---
+++ index.js
@@ -0,0 +1,83 @@
+// TODO: Include packages needed for this application
+
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project (Required)",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter your project title!");
+          return false;
+        }
+      },
+    },
+  ]);
+};
+
+const promptSections = (readmeData) => {
+  console.log(`
+    =================
+    Add a New Section
+    =================
+    `);
+
+  if (!readmeData.projects) {
+    readmeData.projects = [];
+  }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "description",
+        message:
+          "Please provide a description of what the app is for (Required)",
+        validate: (descriptionInput) => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log("You need to enter a description of the app!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "checkbox",
+        name: "TOC",
+        message:
+          "What would you like to add to Table of Contents? (Check all that apply)",
+        choices: [
+          "Installation",
+          "Usage",
+          "License",
+          "Contributing",
+          "Tests",
+          "Questions",
+          "Other",
+        ],
+      },
+    ])
+    .then((projectData) => {
+      readmeData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptSections(readmeData);
+      } else {
+        return readmeData;
+      }
+    });
+};
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
